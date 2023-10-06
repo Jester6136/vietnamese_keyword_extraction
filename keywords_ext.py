0bc51134cea78c98fd71fc5dc43693b9728cd48f
Ba Son <bagiangson2001@gmail.com>
2023-10-06 11:19:22 +0700

    a

--- keywords_ext.py
+++ keywords_ext.py
@@ -12,13 +12,6 @@ def  extracting(document:str,num_keywords:int,lang:str):
         return {"keywords": extractor2.run(document,num_keywords)}
 
 keywords = extracting("""
-
-Đây là bản thiết kế hàng tỉ đô-la của Bộ Quốc phòng mới nhất New Mexico với giá 23 tỉ đô-la của Hợp chủng quốc Hoa Kỳ. Thông báo chính phủ Ấn Độ cho biết Ủy ban quốc phòng đã phê chuẩn 24 thiết bị dự trữ của Bộ Quốc phòng, 21 nước Mỹ đã có một khoản thu nhập cao hơn 7 triệu đô. Theo tuyên bố, số tiền này sẽ được dùng để mua cho quân đội Ấn Độ, xe tăng, vũ khí hạng nhẹ, và máy bay vũ khí hạng nặng để chế tạo bom hạt nhân mới cho không quân Ấn Độ. Hơn nữa, những thiết bị quân sự còn lại trong danh sách quân sự bao gồm các tên lửa của Hải quân, vận chuyển hàng, vận chuyển tàu và máy bay tự động theo thời gian. Nhiều năm qua, các thiết bị của Ấn Độ đã gây ra một căn bệnh lây lan trên đất Ấn Độ, và cả nước Ấn Độ cũng nghi ngờ việc cung cấp vũ khí phòng thủ quốc gia. Những nhà phê bình địa phương cho biết vũ khí sản xuất quốc gia không đủ mạnh, và có rất nhiều cơ hội để tham nhũng. Ít nhất Ấn Độ đã xảy ra bảy vụ mua chuộc hạng nặng từ khi xuất ngũ.
-
-
-
-
-
-""",
+Đây là bản thiết kế hàng tỉ đô-la của Bộ Quốc phòng mới nhất New Mexico với giá 23 tỉ đô-la của Hợp chủng quốc Hoa Kỳ. Thông báo chính phủ Ấn Độ cho biết Ủy ban quốc phòng đã phê chuẩn 24 thiết bị dự trữ của Bộ Quốc phòng, 21 nước Mỹ đã có một khoản thu nhập cao hơn 7 triệu đô. Theo tuyên bố, số tiền này sẽ được dùng để mua cho quân đội Ấn Độ, xe tăng, vũ khí hạng nhẹ, và máy bay vũ khí hạng nặng để chế tạo bom hạt nhân mới cho không quân Ấn Độ. Hơn nữa, những thiết bị quân sự còn lại trong danh sách quân sự bao gồm các tên lửa của Hải quân, vận chuyển hàng, vận chuyển tàu và máy bay tự động theo thời gian. Nhiều năm qua, các thiết bị của Ấn Độ đã gây ra một căn bệnh lây lan trên đất Ấn Độ, và cả nước Ấn Độ cũng nghi ngờ việc cung cấp vũ khí phòng thủ quốc gia. Những nhà phê bình địa phương cho biết vũ khí sản xuất quốc gia không đủ mạnh, và có rất nhiều cơ hội để tham nhũng. Ít nhất Ấn Độ đã xảy ra bảy vụ mua chuộc hạng nặng từ khi xuất ngũ.""",
     10,'vi')
 print(keywords)
